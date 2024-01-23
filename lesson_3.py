d5ad648e0eeaf0e64050d011c722655f8f9f8e13
esandomirsky <sandomirsky.work@gmail.com>
2024-01-23 22:46:36 +0200

    Home_work_l3

---
+++ lesson_3.py
@@ -0,0 +1,21 @@
+
+#########CALCULATOR#########
+
+number_1 = float(input("Enter first number:"))
+function = input("What do you want to do? (+, -, /, *):")
+number_2 = float(input("Enter second number:"))
+
+if function == "+":
+    result_plus = number_1 + number_2
+    print("Result: " + str(result_plus))
+elif function == "-":
+     result_min = number_1 - number_2
+     print("Result: " + str(result_min))
+elif function == "*":
+    result_mul = number_1 * number_2
+    print("Result: " + str(result_mul))
+elif function == "/":
+    result_div = number_1 / number_2
+    print("Result: " + str(result_div))
+else:
+    print("Error!")
