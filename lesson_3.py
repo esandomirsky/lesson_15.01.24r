86a920ce9b78c64dca0d4f41db24111f116c668b
esandomirsky <sandomirsky.work@gmail.com>
2024-01-24 20:56:36 +0200

    Home_work_l3

--- lesson_3.py
+++ lesson_3.py
@@ -22,3 +22,11 @@ elif function == "/":
           print("Cannot divide by zero!")
 else:
     print("Error!")
+
+################LIST############
+
+my_list = [9, 14, 0, " ", 19, [2, 4.5, True], "Hello", 7, 105]
+if len(my_list) > 1:
+    my_list.insert(0, my_list.pop(-1))
+
+print(my_list)
