1961684fefe8feadbb7384a2cbcd8f510ccb305f
esandomirsky <sandomirsky.work@gmail.com>
2024-01-28 14:38:00 +0200

    Home_work_l4

--- lesson_4.py
+++ lesson_4.py
@@ -13,6 +13,8 @@ for number in range(zero_count):
 print(my_list)
 
 ###____2____###
+my_list = [0, 1, 0, 8, 6, 24, 3, 0]
+
 my_list.sort(key=lambda x: x ==0)
 
 print(my_list)
@@ -20,11 +22,20 @@ print(my_list)
 
 #EXERCISE_2
 
+###____1____###
 my_list = [1, 2, 3, 4, 5]
 index = 0
 
 for number in range(0, len(my_list), 2):
     index += my_list[number]
     result = my_list[-1] * index
+
+print(result)
+
+###____2____###
+my_list = [1, 2, 3, 4, 5]
+
+index_sum = sum(my_list[::2])
+result = my_list[-1] * index_sum
 
 print(result)
