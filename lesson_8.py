30b5fd49f2dd987c54a2b6d975e1deac29f3a304
esandomirsky <sandomirsky.work@gmail.com>
2024-02-10 18:34:23 +0200

    Home_work_l8

--- lesson_8.py
+++ lesson_8.py
@@ -18,3 +18,19 @@ assert add_one([9]) == [1, 0], 'Test4'
 print("ОК")
 
 print(add_one([1, 2, 3, 4]))
+
+###___EX_2___###
+
+def find_unique_value(some_list):
+    unique_value = None
+    for num in some_list:
+        if some_list.count(num) == 1:
+            unique_value = num
+            break
+    return unique_value
+
+assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+print("ОК")
+print(find_unique_value([5, 2, 2, 5, 8, 0.222, 8]))
