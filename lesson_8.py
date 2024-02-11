89cb0dd0b4a97bcb7539cb28d304f02087300bf3
esandomirsky <sandomirsky.work@gmail.com>
2024-02-11 21:17:01 +0200

    Home_work_l8

--- lesson_8.py
+++ lesson_8.py
@@ -17,7 +17,7 @@ assert add_one([0]) == [1], 'Test3'
 assert add_one([9]) == [1, 0], 'Test4'
 print("ОК")
 
-print(add_one([1, 2, 3, 4]))
+#print(add_one([1, 2, 3, 4]))
 
 ###___EX_2___###
 
@@ -33,4 +33,4 @@ assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
 assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
 assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
 print("ОК")
-print(find_unique_value([5, 2, 2, 5, 8, 0.222, 8]))
+#print(find_unique_value([5, 2, 2, 5, 8, 0.222, 8]))
